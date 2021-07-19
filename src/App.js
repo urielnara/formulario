ee942aedb7a7f65b87bb3200ef3c81e4b02a97bc
urielnara <87386565+urielnara@users.noreply.github.com>
2021-07-19 09:49:21 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Seminario de titulación 2021</h1>
+      <h2>Registro de asistencia</h2>
+    </div>
+  );
+}
