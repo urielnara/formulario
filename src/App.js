1440bef6accffb87d2d1ba5444cff1f01a04e172
urielnara <87386565+urielnara@users.noreply.github.com>
2021-07-19 09:58:07 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Seminario de titulación 2021</h1>
-      <h2>Registro de asistencia</h2>
+      <h1>Registro</h1>
     </div>
   );
 }
